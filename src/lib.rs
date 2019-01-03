12559b3f411fbe015437ee02ac4e378d473b1fba
Niall Moore <niall@hycon.io>
2019-01-03 16:37:09 +0900

    <WIP> Updated schema and added status struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate capnp;
+extern crate capnp_rpc;
 pub mod test_schema_capnp {
     include!(concat!("schema", "/test_schema_capnp.rs"));
 }
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,18 +1,50 @@
-use crate::test_schema_capnp::status;
+use crate::test_schema_capnp::{network, status};
+use capnp::capability::Promise;
+use capnp::message::Builder;
 
-pub struct RPCServer {
+pub struct StatusImpl {
+    version: i32,
     guid: String,
+    public_port: i32,
+    network_id: String,
+    port: i32,
+}
+
+impl StatusImpl {
+    fn new(version: i32, public_port: i32, network_id: String, port: i32) -> StatusImpl {
+        StatusImpl {
+            version,
+            public_port,
+            network_id,
+            port,
+            guid: RPCServer::generate_guid(),
+        }
+    }
+}
+pub struct RPCServer {
+    status: StatusImpl,
 }
 
 impl RPCServer {
     fn new() -> RPCServer {
         RPCServer {
-            guid: RPCServer::generate_guid(),
+            status: StatusImpl::new(1, 8148, String::from("hycon"), 8148),
         }
     }
 
     fn generate_guid() -> String {
         String::from("afhwjgbfdjobnqfjdfqojgadnv")
+    }
+}
+
+impl network::Server for RPCServer {
+    fn get_status(
+        &mut self,
+        params: network::GetStatusParams,
+        mut results: network::GetStatusResults,
+    ) -> Promise<(), ::capnp::Error> {
+        println!("Received Status Call");
+        Promise::ok(())
     }
 }
 
@@ -23,6 +55,9 @@ pub mod tests {
     #[test]
     fn it_should_do_stuff() {
         let server = RPCServer::new();
-        assert_eq!(server.guid, String::from("afhwjgbfdjobnqfjdfqojgadnv"));
+        assert_eq!(
+            server.status.guid,
+            String::from("afhwjgbfdjobnqfjdfqojgadnv")
+        );
     }
 }
